e771832c808080fe8f144917b2936d385ed39e95
cho6019 <j01040864205@gmail.com>
2025-01-08 17:49:51 +0900

    기능수정

--- api/index.py
+++ api/index.py
@@ -3,6 +3,7 @@ from datetime import datetime, date
 from typing import Dict
 import random
 
+
 ### Create FastAPI instance with custom docs and openapi url
 app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
 
@@ -18,12 +19,18 @@ def age_calculator(birthday: str) -> Dict[str, str]:
     :param birthday: 생년월일 (형식: YYYY-MM-DD)
     :return: 생년월일 및 만나이를 포함한 JSON 응답
     """
-    random_age = random.randint(0, 100)
-    today = date.today()
+    #random_age = random.randint(0, 100)
     
+    today = date.today()
+    birth_date = datetime.strptime(birthday, "%Y-%m-%d").date()
+
+    
+    age = today.year - birth_date.year
+    # TODO 아직 생일 여부에 대한 기능 없
+
     return {
             "birthday": birthday,
-            "age": str(26),
+            "age": str(age),
             "basedate": str(today),
             "message": "Age calculated successfully!"
             }
